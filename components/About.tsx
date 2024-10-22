a2c4b253bf8dd46a50c727a4ab7a16818dcc5384
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-22 22:51:50 +0500

    solving error

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
         </div>
         <div className=' w-[500px] flex flex-col justify-center gap-10 px-5'>
             <h3 className='text-gray-500 text-5xl '>About</h3>
-            <p className='text-xl'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <p className='text-xl'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             
             <button className='flex justify-center bg-white w-40 py-5 items-center gap-2 '>READ MORE <FaLongArrowAltRight className='text-3xl font-extralight'/></button>
         </div>
