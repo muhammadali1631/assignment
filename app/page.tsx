290cedd4da8dbfc8e86d8713115ee329a92127a9
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-22 22:45:05 +0500

    solving error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import BackGround from '@/components/BackGround'
 import  Main  from '@/components/Main'
 import About from '@/components/About'
 import Mission from '@/components/Mission'
